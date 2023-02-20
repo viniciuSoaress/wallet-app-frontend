d250d422430fe05bfea07d86302790a636404ae2
viniciszs <114860862+viniciszs@users.noreply.github.com>
2023-02-20 09:07:01 -0800

    integrate user initial validation

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-const onClickLogin = () => {
+const validateUser = async (email) => {
+   try{
+    const result = await fetch(`https://mp-wallet-app-api.herokuapp.com/users?email=${email}`);
+    const user = await result.json();
+    return user;
+   }catch (error) {
+    return { error };
+   }
+}
+
+const onClickLogin = async () => {
     const email = document.getElementById('input-email').value;
-    if(email.length < 5 && !email.includes('@')){
+    if (email.length < 5 && !email.includes('@')) {
         alert("E-mail não e valido");
         return;
+    };
+    const result = await validateUser(email);
+    if(result.error){
+        alert('falha ao validare-mail.');
+        return;
     }
-    localStorage.setItem('@WalletApp:userEmail', email);
+    localStorage.setItem('@WalletApp:userEmail', result.email);
+    localStorage.setItem('@WalletApp:userName', result.name);
+    localStorage.setItem('@WalletApp:userId', result.id);
     window.open('./src/pagens/home/index.html', '_self');
 };
