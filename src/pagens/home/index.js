302dae95e74885b19c7d79cdfe1ce26b89bf3f28
viniciszs <114860862+viniciszs@users.noreply.github.com>
2023-02-20 11:47:25 -0800

    doing navigation bar on the home page with JavaScrip functions

--- src/pagens/home/index.js
+++ src/pagens/home/index.js
@@ -1,3 +1,30 @@
+const onLoadUserInfo = () =>{
+
+    const email = localStorage.getItem('@WalletApp:userEmail') 
+    const name = localStorage.getItem('@WalletApp:userName') ;
+
+    const navbarUserInfo = document.getElementById('navbar-user-conteiner');
+    const navbarUserAvatar = document.getElementById('navbar-user-avatar');
+
+    const emailElement = document.createElement('p');
+    const emailText = document.createTextNode(email);
+    emailElement.appendChild(emailText);
+
+    navbarUserInfo.appendChild(emailElement);
+
+    const logoutElement = document.createElement('a');
+    const logoutText = document.createTextNode('Sair');
+    logoutElement.appendChild(logoutText);
+
+    navbarUserInfo.appendChild(logoutElement)
+
+    const nameElement = document.createElement('h3');
+    const nameText = document.createTextNode(name.charAt(0));
+    nameElement.appendChild(nameText);
+    navbarUserAvatar.appendChild(nameElement)
+}
+
 window.onload = () =>{
     const email = localStorage.getItem('@WalletApp:userEmail');
+    onLoadUserInfo()
 }
